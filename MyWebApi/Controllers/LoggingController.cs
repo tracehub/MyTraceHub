2880d8cd9116942b8e9cacf15790bac9481f9148
admin <admin@fonlow.com>
2016-08-23 15:38:43 +1000

    MyWebApi working

---
+++ MyWebApi/Controllers/LoggingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fonlow.Web.Logging.Controllers
+{
+    [Authorize(Roles = "API")]
+    public class LoggingController : Controller
+    {
+        // GET: LoggingView
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
