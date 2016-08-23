6f504c9a23c3812a2361531aa40335587902b072
admin <admin@fonlow.com>
2016-08-23 14:05:37 +1000

    initial

---
+++ MyMVC/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMVC.Startup))]
+namespace MyMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            app.MapSignalR();
+        }
+    }
+}
